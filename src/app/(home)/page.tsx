4464af3aaac45c389707f30ba8ba90f9c38a508f
ADIL DYER <smile.itsadil@gmail.com>
2024-09-04 23:57:30 +0530

    checking vercel deployment

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -60,7 +60,7 @@ const Home = () => {
       </div>
       <div className="textDiv">
         <h4>
-          QBON, “The Question Bank of NFSU” is your go-to platform for all
+          QBbON, “The Question Bank of NFSU” is your go-to platform for all
           things academic. 📚✨🧠
         </h4>
         <br />
